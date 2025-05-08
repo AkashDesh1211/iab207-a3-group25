dbdf36281ec26c51a207d3bdcf408fa09ee2aee8
PrishP <prishvith@gmail.com>
2025-05-08 12:05:47 +1000

    Add files via upload

--- a3_group25/STEMWorkshops/views.py
+++ a3_group25/STEMWorkshops/views.py
@@ -6,3 +6,19 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
+@main_bp.route('/history')
+def history():
+    return render_template('booking_history.html')
+
+@main_bp.route('/create')
+def create():
+    return render_template('create_event.html')
+
+@main_bp.route('/events')
+def events():
+    return render_template('event_category.html')
+
+@main_bp.route('/details')
+def details():
+    return render_template('event_details.html')
