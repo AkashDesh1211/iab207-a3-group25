4e6ef0bda18c09963f5c5dd57515dc8d70517202
PrishP <prishvith@gmail.com>
2025-05-08 11:48:37 +1000

    add routes and new templates
    Added most of the html pages with new links, as well as more routes. Currently empty, will work on backend functionality later.

---
+++ a3_group25/views.py
@@ -0,0 +1,24 @@
+from flask import Blueprint
+from flask import render_template, request, redirect, flash
+
+main_bp = Blueprint('main', __name__)
+
+@main_bp.route('/')
+def index():
+    return render_template('index.html')
+
+@main_bp.route('/history')
+def history():
+    return render_template('booking_history.html')
+
+@main_bp.route('/create')
+def create():
+    return render_template('create_event.html')
+
+@main_bp.route('/events')
+def events():
+    return render_template('event_category.html')
+
+@main_bp.route('/details')
+def details():
+    return render_template('event_details.html')
