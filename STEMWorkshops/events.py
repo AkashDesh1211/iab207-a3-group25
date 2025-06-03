776b8296f6155962f113213bcc25b400b1d29525
PrishP <prishvithp@gmail.com>
2025-06-03 18:36:29 +1000

    add required imports and functions to events and forms.py

--- STEMWorkshops/events.py
+++ STEMWorkshops/events.py
@@ -43,3 +43,17 @@ def create_event():
         return redirect(url_for('new_event.create_event'))
 
     return render_template('create_event.html', new_event=new_event )
+
+def check_upload_file(form):
+  # get file data from form  
+  fp = form.image.data
+  filename = fp.filename
+  # get the current path of the module file… store image file relative to this path  
+  BASE_PATH = os.path.dirname(__file__)
+  # upload file location – directory of this file/static/image
+  upload_path = os.path.join(BASE_PATH, 'static/image', secure_filename(filename))
+  # store relative path in DB as image location in HTML is relative
+  db_upload_path = '/static/image/' + secure_filename(filename)
+  # save the file and return the db upload path
+  fp.save(upload_path)
+  return db_upload_path
