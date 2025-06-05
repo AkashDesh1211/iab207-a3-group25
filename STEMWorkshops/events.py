78b1157a817bb11e5be53da70607f90af58c5732
alarme21 <alarme@optusnet.com.au>
2025-06-05 18:14:38 +1000

    we didn't need the all

--- STEMWorkshops/events.py
+++ STEMWorkshops/events.py
@@ -27,7 +27,7 @@ def show(id):
 @events_bp.route('/history')
 @login_required
 def booking_history():
-    bookings = db.session.scalar(db.select(Order).where(Order.user_id==current_user.user_id)).all()
+    bookings = db.session.scalar(db.select(Order).where(Order.user_id==current_user.user_id))
     
     return render_template('booking_history.html', bookings=bookings)
 
