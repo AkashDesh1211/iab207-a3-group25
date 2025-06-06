968b9c9b90ac16830537e8bc3fd472895e274846
kmason19 <11448733@qut.edu.au>
2025-06-06 17:02:31 +1000

    booked event for testing

--- STEMWorkshops/events.py
+++ STEMWorkshops/events.py
@@ -112,7 +112,7 @@ def booking(id):
       flash('Successfully booked event')
       return redirect(url_for('events.booking_history')) 
    
-   return render_template('user.html', form=booking_form, id=id, heading='Book Ticket')
+   return render_template('book_tickets.html', form=booking_form, id=id, heading='Book Ticket')
 
 
 
